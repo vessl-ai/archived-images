44287c4469a703643194a4941355fb9a0685fd3f
Jaeman An <ajmbell@gmail.com>
2020-06-13 20:20:20 +0900

    add cors

--- jupyter-base/start-notebook.sh
+++ jupyter-base/start-notebook.sh
@@ -5,7 +5,7 @@
 set -e
 
 if [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
-  . /usr/local/bin/start.sh $wrapper jupyter lab --NotebookApp.ip=0.0.0.0 --NotebookApp.token=${SAVVI_TOKEN} --no-browser "$@"
+  . /usr/local/bin/start.sh $wrapper jupyter lab --NotebookApp.ip=0.0.0.0 --NotebookApp.token=${SAVVI_TOKEN} --NotebokApp.base_url=${SAVVI_BASE_URL} --NotebookApp.allow_origin=* --no-browser "$@"
 else
-  . /usr/local/bin/start.sh $wrapper jupyter notebook --NotebookApp.ip=0.0.0.0 --NotebookApp.token=${SAVVI_TOKEN} --no-browser "$@"
+  . /usr/local/bin/start.sh $wrapper jupyter notebook --NotebookApp.ip=0.0.0.0 --NotebookApp.token=${SAVVI_TOKEN} --NotebookApp.base_url=${SAVVI_BASE_URL} --NotebookApp.allow_origin=* --no-browser "$@"
 fi
