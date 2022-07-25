637533f0c1b6b7486bb53dd03830cc3a54500b0e
Seokju Hong <seokju@vessl.ai>
2022-07-25 11:03:19 +0900

    [VSSL-3537] Move dockerfile endpoint from ecr-public to quay, and force platform to linux/amd64

--- deploy.py
+++ deploy.py
@@ -20,14 +20,14 @@ def docker_deploy(dockerhub_repo, tag, push, dockerfile_dir):
     print(f'Building {dockerfile_path}')
 
     tag_ref = "{}:{}".format(dockerhub_repo, tag)
-    run(f'docker build -t {tag_ref} -f {dockerfile_path} {dockerfile_dir}')
+    run(f'docker build --platform=linux/amd64 -t {tag_ref} -f {dockerfile_path} {dockerfile_dir}')
     if push:
         run(f'docker push {tag_ref}')
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dockerhub', type=str, default='public.ecr.aws/vessl/kernels', help='Docker Hub repository')
+    parser.add_argument('--dockerhub', type=str, default='quay.io/vessl-ai/kernels', help='Docker Hub repository')
     parser.add_argument('-t', '--tag', type=str, default='latest', help='tagging docker image')
     parser.add_argument('DIR', metavar='DIR', type=str, help='Dockerfile directory')
     parser.add_argument('-p', '--push', default=False, action='store_true', help='Push image to Docker Hub T/F')
